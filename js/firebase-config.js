3a7024599e143e2dfc84577dab781e86104ed35b
brunavaldez <bvaldez12774@scis-bo.com>
2026-06-01 22:45:53 -0400

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration for Nutrift MVP
+const firebaseConfig = {
+  apiKey: "AIzaSyD6i25DDQUnkfCW8fXoSg6ZrsvogItMqpQ",
+  authDomain: "nutrifit-app-9357e.firebaseapp.com",
+  projectId: "nutrifit-app-9357e",
+  storageBucket: "nutrifit-app-9357e.firebasestorage.app",
+  messagingSenderId: "1067458036919",
+  appId: "1:1067458036919:web:1d03f329a7102deed23932",
+  measurementId: "G-SKJXQRH0KV"
+};
+
+export { firebaseConfig };
